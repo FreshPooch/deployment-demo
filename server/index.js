5d6e2446643cd19c02f92ae50a01eb876067e4a9
Nate Pucci <nathaniel.pucci@gmail.com>
2022-07-27 12:55:28 -0400

    try 2

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
 const app = express()
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 const port = process.env.PORT || 4005
